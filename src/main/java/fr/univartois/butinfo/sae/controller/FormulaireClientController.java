48061ba2dbabf60934a14ce5d02b56470764655a
Thérage Ludovic <therage.ludovic28@gmail.com>
2025-06-11 15:24:49 +0200

    Main page (Accueil) de l'entreprise O-De-France avec les 3 autres controllers (Client, Commmande, Stock, Formulaire)

--- src/main/java/fr/univartois/butinfo/sae/controller/FormulaireClientController.java
+++ src/main/java/fr/univartois/butinfo/sae/controller/FormulaireClientController.java
@@ -1,11 +1,20 @@
 package fr.univartois.butinfo.sae.controller;
 
+import fr.univartois.butinfo.sae.HelloApplication;
 import fr.univartois.butinfo.sae.model.*;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FormulaireClientController {
 
@@ -139,5 +148,25 @@ public class FormulaireClientController {
 	@FXML
 	public void annuler() {
 		champTelephone.getScene().getWindow().hide();
+	}
+
+	public static void changerVue(Stage stage, String fxml) {
+		try {
+			FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource(fxml));
+			Parent root = loader.load();
+			stage.setScene(new Scene(root, 900, 540));
+		} catch (IOException e) {
+			System.err.println("Erreur lors du chargement de " + fxml);
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	private void onClickButtonMainPage(ActionEvent event) {
+		// Obtenir le bouton qui a déclenché l'événement
+		Button button = (Button) event.getSource();
+		// Obtenir la scène à partir du bouton
+		Stage stage = (Stage) button.getScene().getWindow();
+		changerVue(stage, "view/Accueil-view.fxml");
 	}
 }
--- src/main/java/fr/univartois/butinfo/sae/controller/StockEauController.java
+++ src/main/java/fr/univartois/butinfo/sae/controller/StockEauController.java
@@ -1,10 +1,18 @@
 package fr.univartois.butinfo.sae.controller;
 
+import fr.univartois.butinfo.sae.HelloApplication;
 import fr.univartois.butinfo.sae.model.StockEau;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StockEauController {
 
@@ -66,5 +74,25 @@ public class StockEauController {
 
     public void setStockList(ObservableList<StockEau> list) {
         stockList.setAll(list);
+    }
+
+    public static void changerVue(Stage stage, String fxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource(fxml));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root, 900, 540));
+        } catch (IOException e) {
+            System.err.println("Erreur lors du chargement de " + fxml);
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onClickButtonMainPage(ActionEvent event) {
+        // Obtenir le bouton qui a déclenché l'événement
+        Button button = (Button) event.getSource();
+        // Obtenir la scène à partir du bouton
+        Stage stage = (Stage) button.getScene().getWindow();
+        changerVue(stage, "view/Accueil-view.fxml");
     }
 }
