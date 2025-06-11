0bcd2e5bded99e678116c3e1383d5f14e396669b
Matheo Popieul <matheo_popieul@ens.univ-artois.fr>
2025-06-11 10:14:03 +0200

    static int essaie

--- src/main/java/fr/univartois/butinfo/sae/model/Commande.java
+++ src/main/java/fr/univartois/butinfo/sae/model/Commande.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class Commande implements RemisesPossibles {
     private final int id;
-    private static int essaie =20;
 
     private static int nextId = 0;
     private final ObservableList<LigneDeCommande> lignesDeCommande;
